28dcb1ae53a9d751840007359f266a37c76b5e18
Igor Raigorodskyi <raigorodskyi@gmail.com>
2025-08-07 12:17:23 -0400

    Fixed confirm and error messages for the user

--- financial-dash/src/app/stocks-view/stocks-view.component.ts
+++ financial-dash/src/app/stocks-view/stocks-view.component.ts
@@ -28,6 +28,7 @@ modalType: 'buy' | 'sell' = 'buy';
 selectedStock: any = null;
 modalQuantity: number = 1;
 bankAccounts: BankAccount[] = [];
+errorMessage: string = '';
 selectedBankAccount: BankAccount | null = null;
 response = '';
 
@@ -61,16 +62,17 @@ constructor(@Inject(PLATFORM_ID) private platformId: Object, private portfolioSe
 
   getMarketStock() {
     const query = this.searchQuery.toUpperCase();
-    this.portfolioService.getStockByTicker(query).subscribe((stockData) => {
-      this.marketStockValues = stockData;
-      console.log(this.marketStockValues);
+    this.portfolioService.getStockByTicker(query).subscribe({
+      next: (stockData) => {
+        this.marketStockValues = stockData;
+        console.log(this.marketStockValues);
+        this.errorMessage = '';
+    },
+    error: (err) => {
+      console.error('Error fetching stock:', err);
+      this.errorMessage = 'Could not fetch stock. Please check the ticker or try again later.';
+    }
 
-      // Calculate stocks total dynamically
-      this.stocksValuation = this.stockList.reduce(
-        (sum, stock) => sum + stock.data.current_price * stock.data.shares,
-        0
-      );
-      console.log(this.marketStockList);
     });
   }
 
@@ -149,7 +151,7 @@ constructor(@Inject(PLATFORM_ID) private platformId: Object, private portfolioSe
         })
     }
   
-    this.closeModal();
+    setTimeout(() => this.closeModal(), 2000);
   }
 
   
